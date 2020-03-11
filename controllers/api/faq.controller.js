a2556a59ab7a43e79bd52ca966bc1372db229c34
JOAO VICTOR FANCHINI DE AZEVEDO <082160015@ftt.cefsa.edu.br>
2020-03-10 22:09:10 -0300

    Criação do controller de faq

---
+++ controllers/api/faq.controller.js
@@ -0,0 +1,68 @@
+var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+
+var faqService = require('services/faq.service')
+
+// routes
+router.post('/register', registerFaq);
+router.get('/:_id', getCurrentFaq);
+router.put('/:_id', updateFaq);
+router.delete('/:_id', deleteFaq);
+router.get('/',getAllFaq);
+
+module.exports = router;
+
+function getAllFaq(req,res){
+    faqService.getAll()
+        .then(function (users) {
+            res.send(users);
+        })
+        .catch(function (err){
+            res.status(400).send(err);
+        });
+}
+
+function registerFaq(req, res) {
+    faqService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentFaq(req, res) {
+    faqService.getById(req.params._id)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateFaq(req, res) {
+    faqService.update(req.params._id, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteFaq(req, res) {
+    faqService.delete(req.params._id)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
